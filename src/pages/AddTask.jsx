e961745c79796f88e0f4f0f6866d959f243c1ddf
christianzaboli <zabolichristian@gmail.com>
2026-01-07 19:00:58 +0100

    form add Task creato con campi validati e non, lieve stile applicato, milestone 5 completato

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,9 +1,52 @@
+import { useState, useRef } from "react";
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~";
+
 export default function AddTask() {
+  const [title, setTitle] = useState("");
+  const descRef = useRef();
+  const statusRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title.trim() === "") {
+      console.error("Compila il campo Nome del task");
+      return;
+    }
+    if ([...title].some((char) => symbols.includes(char))) {
+      console.error("Il campo Nome del task contiene caratteri non validi");
+      return;
+    }
+    console.log({
+      title,
+      description: descRef.current.value,
+      status: statusRef.current.value,
+    });
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form className="tasks-form">
+        <label>Nome del task:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          autoFocus
+        />
+        <label>Descrizione:</label>
+        <textarea name="Descrizione" ref={descRef}></textarea>
+        <label>
+          Stato:
+          <select name="status" ref={statusRef}>
+            <option value="To do">To do</option>
+            <option value="Doing">Doing</option>
+            <option value="Done">Done</option>
+          </select>
+        </label>
       </form>
+      <button type="submit" onClick={handleSubmit}>
+        Aggiungi Task
+      </button>
     </div>
   );
 }
